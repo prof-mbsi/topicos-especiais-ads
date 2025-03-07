a3645dcfc775f62976e7e358c68967c1b7af0732
Marlon <marlonbsi@protonmail.com>
2025-03-07 16:41:21 -0300

    Criação do primeiro projeto em C# .NET

---
+++ MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+        Console.WriteLine("Hello, World! ;) ");
+
+        // Solicita o nome do usuário
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        // Solicita a idade do usuário
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        // Exibe uma mensagem personalizada
+        Console.WriteLine($"\nOlá, {nome}! Você tem {idade} anos.");
+
+    }
+
+}
