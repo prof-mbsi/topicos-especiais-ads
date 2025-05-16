10fbe3e3efe217d52b9491ccb425ed498bc7f0ac
Marlon <marlonbsi@protonmail.com>
2025-05-16 16:33:34 -0300

    Adicionando aula10

---
+++ aula10/aula1605/Models/Livro.cs
@@ -0,0 +1,9 @@
+
+namespace aula1605.Models {
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+    }
+}
